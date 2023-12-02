baa58679cfb16e61cdc2d118648dc5d3fa04462d
Pieter Brandwijk <pieter@pbrandwijk.com>
2023-12-02 22:26:53 +0100

    Add test data

---
+++ test/ExampleBusinessLibrary.js
@@ -0,0 +1,29 @@
+/**
+ * @fileOverview An example file with code as found in a STEP Business Library.
+ * @module Libraries/ExampleBusinessLibrary
+ */
+
+/** Standard log prefix */
+const log_prefix = "[ExampleBusinessLibrary] ";
+
+/**
+ * Example function that returns a boolean.
+ * @type Validation check
+ * @level Warning
+ * @objecttype Product
+ * @statusrange 4-25
+ */
+function exampleLibraryFunction(node, manager, referenceTarget) {
+    return true;
+}
+
+/**
+ * Example function that returns the sum of the two parameters.
+ * @section Generic functions
+ * @param {Int} a The first parameter
+ * @param {Int} b The second parameter
+ * @returns {Int} The sum of the two parameters
+ */
+function exampleGenericFunction(a, b) {
+    return a + b;
+}
