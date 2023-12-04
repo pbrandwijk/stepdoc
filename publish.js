95c78d999e887ef9347f49930c91e2d8b444748e
Pieter Brandwijk <pieter@pbrandwijk.com>
2023-12-04 13:53:26 +0100

    Add main file

---
+++ publish.js
@@ -0,0 +1,12 @@
+/** @module publish */
+
+/**
+ * Generate documentation output.
+ *
+ * @param {TAFFY} data - A TaffyDB collection representing all the symbols documented in the code.
+ * @param {object} opts - An object with options information.
+ */
+exports.publish = function (data, opts) {
+	console.log("Starting STEPDoc generation");
+
+};
